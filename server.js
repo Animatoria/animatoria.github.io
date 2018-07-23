774f03591908b8497c43f312ae97acedf9f840e3
Animatoria <nn.animatoria@gmail.com>
2018-07-23 19:10:47 +0300

    http

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var https = require('https');
+var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-https.createServer(function (req, res) {
+http.createServer(function (req, res) {
 	var q = url.parse(req.url, true);
 	var way = './public', cont;
 
